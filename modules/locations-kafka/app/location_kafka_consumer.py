3e91160c078dfa23df46f434aae2113471f02697
toan la <ldtoan1306@gmail.com>
2022-09-12 12:37:20 +0700

    upate github actions, expose locations-api port dockerfile

--- modules/locations-kafka/app/location_kafka_consumer.py
+++ modules/locations-kafka/app/location_kafka_consumer.py
@@ -5,7 +5,7 @@ from services import LocationService
 TOPIC_NAME = 'test-topic'
 # KAFKA_SERVER = 'localhost:9092'
 KAFKA_SERVER = 'kafka.default.svc.cluster.local'
-GROUP_ID = 'console-consumer-10389'
+GROUP_ID = 'console-consumer-53977'
 
 consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=KAFKA_SERVER, group_id=GROUP_ID)
 for message in consumer:

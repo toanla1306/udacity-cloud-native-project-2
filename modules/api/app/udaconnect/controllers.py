0d4843ffdff1bfc2e4e2cdcfaa3b0bfafb3af038
toan la <ldtoan1306@gmail.com>
2022-09-13 20:27:51 +0700

    remove tag todo and update image infrastructure

--- modules/api/app/udaconnect/controllers.py
+++ modules/api/app/udaconnect/controllers.py
@@ -17,8 +17,6 @@ DATE_FORMAT = "%Y-%m-%d"
 api = Namespace("UdaConnect", description="Connections via geolocation.")  # noqa
 
 
-# TODO: This needs better exception handling
-
 
 @api.route("/locations")
 @api.route("/locations/<location_id>")
